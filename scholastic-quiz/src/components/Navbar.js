596ea38e43ab171a20d080230dafe2dd0ed13da8
Sarthak <sarthakbh321@gmail.com>
2020-05-22 16:32:50 +0530

    added login button

--- scholastic-quiz/src/components/Navbar.js
+++ scholastic-quiz/src/components/Navbar.js
@@ -1,15 +1,18 @@
 import React from "react";
-import {AppBar, Typography, Toolbar} from "@material-ui/core";
+import {AppBar, Typography, Toolbar, Button} from "@material-ui/core";
 import styles from './Navbar.css';
 
 function Navbar() {
 	return (
-		<AppBar position="static" className="navbar">
-			<Toolbar>
-				<Typography variant="h6" className="nav-logo"> SCHOLASTIC | </Typography>
-				<Typography varirant="h6" className="nav-head">The Hunger Games Quiz</Typography>
-			</Toolbar>
-		</AppBar>
+		<div className="root">
+			<AppBar position="static" className="navbar">
+				<Toolbar>
+					<Typography variant="h6" className="nav-logo"> SCHOLASTIC | </Typography>
+					<Typography varirant="h6" className="nav-head">The Hunger Games Quiz</Typography>
+					<Button color="inherit" className="login">Login</Button>
+				</Toolbar>
+			</AppBar>
+		</div>
 	);
 }
 
