f633074cd24ece3a865efef2f788f03adc89c3fb
Sarthak <sarthakbh321@gmail.com>
2020-05-23 00:23:59 +0530

    started login page

--- scholastic-quiz/src/pages/LoginPage.js
+++ scholastic-quiz/src/pages/LoginPage.js
@@ -1,10 +1,57 @@
 import React from 'react';
+import './LoginPage.css';
+import { Container, TextField, withStyles, withTheme } from "@material-ui/core";
+
+const Text = withStyles({
+	root: {
+		'& label': {
+			color: 'white',
+		},
+		'& label.Mui-focused': {
+			color: 'orange',
+		},
+		'& .MuiInput-underline:after': {
+			borderBottomColor: 'orange',
+		},
+		'& .MuiInputBase-input': {
+			color: 'white',
+		},
+		'& .MuiOutlinedInput-root': {
+			'& fieldset': {
+			  borderColor: 'rgba(255,255,255, 0.3)',
+			  color: 'white',
+			},
+			'&:hover fieldset': {
+			  borderColor: 'orange',
+			},
+			'&.Mui-focused fieldset': {
+			  borderColor: 'orange',
+			},
+		  },
+	}
+})(TextField);
 
 function LoginPage() {
 	return (
-		<div>
-			<h1>Login Page</h1>
-		</div>
+		<Container className="login-page">
+			<div className="login-form">
+				<form className="form">
+					<Text
+						id="email"
+						label="Email"
+						type="email"
+						className="form-input"
+						variant="outlined"></Text>
+					<br />
+					<Text
+						id="password"
+						type="password"
+						label="Password"
+						className="form-input"
+						variant="outlined"></Text>
+				</form>
+			</div>
+		</Container>
 	)
 }
 
