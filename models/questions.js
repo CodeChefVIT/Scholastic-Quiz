700cbbc24913fc3f6f425925ecc29f4c9453d306
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-05-23 15:43:48 +0530

    pull

--- models/questions.js
+++ models/questions.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const questionSchema = new mongoose.Schema({
-    description: String,
+    description:{
+        type: String,
+        required:true
+    },
     alternatives: [
         {
             text: {
@@ -10,10 +13,7 @@ const questionSchema = new mongoose.Schema({
             },
         }
     ],
-    correct_answer:{
-        type : String,
-    
-    }
+    correct_answer:String
 })
 
 module.exports = mongoose.model('question', questionSchema)
