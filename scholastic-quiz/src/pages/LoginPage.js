a08bb2f5f5c58a802d9d33eeb5073f06bd71c07a
Sarthak <sarthakbh321@gmail.com>
2020-05-23 11:27:44 +0530

    completed login page UI

--- scholastic-quiz/src/pages/LoginPage.js
+++ scholastic-quiz/src/pages/LoginPage.js
@@ -1,35 +1,9 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
 import { Container, TextField, withStyles, Typography, Button } from "@material-ui/core";
+import {Link} from "react-router-dom";
+import TextInput from "../components/TextInput";
 
-const Text = withStyles({
-	root: {
-		'& label': {
-			color: 'rgba(0,0,0, 0.7)',
-		},
-		'& label.Mui-focused': {
-			color: 'orange',
-		},
-		'& .MuiInput-underline:after': {
-			borderBottomColor: 'orange',
-		},
-		'& .MuiInputBase-input': {
-			color: 'black',
-		},
-		'& .MuiOutlinedInput-root': {
-			'& fieldset': {
-			  borderColor: 'rgba(0,0,0, 0.3)',
-			  color: 'black',
-			},
-			'&:hover fieldset': {
-			  borderColor: 'orange',
-			},
-			'&.Mui-focused fieldset': {
-			  borderColor: 'orange',
-			},
-		  },
-	}
-})(TextField);
 
 function LoginPage() {
 	const [email, changeEmail] = useState("");
@@ -38,26 +12,32 @@ function LoginPage() {
 		changeEmail(event.target.value);
 	}
 
+	const handleSubmit = (event) => {
+		event.preventDefault();
+	}
+
 	return (
 		<Container className="login-page">
 			<div className="login-form">
 				<Typography variant="h3" color="primary" className="login-head">Login Now!</Typography><br />
 				<form className="form">
-					<Text
+					<TextInput
 						id="email"
 						label="Email"
 						type="email"
 						className="form-input"
 						variant="outlined"
-						onChange={handleEmailChange}></Text>
+						onChange={handleEmailChange}></TextInput>
 					<br />
-					<Text
+					<TextInput
 						id="password"
 						type="password"
 						label="Password"
 						className="form-input"
-						variant="outlined"></Text>
+						variant="outlined"></TextInput>
 				</form>
+				<Button className="login-btn" onClick={handleSubmit}>Login</Button>
+				<Link to="/register" className="link register-link">Don't have an account? Join the rebellion now!</Link>
 			</div>
 		</Container>
 	)
