46d88df9f491dd51aa943e284b25827d53b72a29
Vinayak Gupta <guptavinayakg@gmail.com>
2020-05-23 15:04:02 +0530

    Errors

--- src/components/PlayMenuBar.js
+++ src/components/PlayMenuBar.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 import './PlayMenuBar.css';
 
 function PlayMenuBar() {
-	const [isLoggedIn, setLoggedIn] = useState(true);
+	const [isLoggedIn, setLoggedIn] = useState(false);
 
 	if(!isLoggedIn) {
 		return (
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -5,15 +5,15 @@ import PlayMenuBar from '../components/PlayMenuBar';
 
 function Welcome() {
 	return (
-		<Container>
+		<Container style={{height: 'calc(100vh - 56px)', display: 'flex', justifyContent: 'center'}}>
 			<div className="welcome-screen">
-				<Grid container spacing={3}>
+				<Grid container spacing={0}>
 					<Grid item xs={12} md={6} className="heading-section">
-						<img src="quiz-head.png" className="quiz-image"></img>
+						<img src="quiz-head.png" className="quiz-image" alt=""></img>
 					</Grid>
 					<Hidden smDown>
 						<Grid item xs={12} md={6} className="pin-section">
-							<img src="hg-pin.png" className="pin-image"></img>
+							<img src="hg-pin.png" className="pin-image" alt=""></img>
 						</Grid>
 					</Hidden>
 				</Grid>
