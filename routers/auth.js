e589a76e4ab6b65f1173d7be8172959b253e479e
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2020-05-23 12:43:25 +0530

    query

--- routers/auth.js
+++ routers/auth.js
@@ -19,18 +19,18 @@ router.post('/register',async (req,res)=>{
     
 
     //check for  existing user
-    const emailExist =await User.findOne({email:req.body.email})
+    const emailExist =await User.findOne({email:req.query.email})
     if(emailExist){
         return res.status(400).send('Email Exists')
     }
 
     //hash the password
     const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(req.body.password,salt)
+    const hashedPassword = await bcrypt.hash(req.query.password,salt)
 
     const user = new User({
-        name : req.body.name,
-        email : req.body.email,
+        name : req.query.name,
+        email : req.query.email,
         password: hashedPassword
     })
     try{
