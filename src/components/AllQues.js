74efdfed1d6fa1d0f9f95ee999ba0725b307a93d
Vinayak Gupta <guptavinayakg@gmail.com>
2020-05-25 23:45:17 +0530

    admin page fix

--- src/components/AllQues.js
+++ src/components/AllQues.js
@@ -1,35 +1,14 @@
 import React from 'react';
 import MaterialTable from "material-table";
+import {Alert} from "@material-ui/lab";
 import Loading from "../pages/Loading";
 import axios from "axios";
-import {
-	Button, Dialog, DialogActions,
-	DialogContent, DialogTitle, 
-	Radio, RadioGroup,
-	FormControlLabel, FormControl,
-	FormLabel, Grid
-} from '@material-ui/core';
-import TextInput from "../components/TextInput";
-import { Redirect } from 'react-router';
+import {Grid} from "@material-ui/core";
 
 export default function AllQues(){
 
-	const [open, setOpen] = React.useState(false);
-	const [value, setValue] = React.useState('none');
-	const [valueError, setValueError] = React.useState('');
-	const [ques, setQues] = React.useState('');
-	const [quesError, setQuesError] = React.useState('');
-	const [op1, setOp1] = React.useState('');
-	const [op1Error, setOp1Error] = React.useState('');
-	const [op2, setOp2] = React.useState('');
-	const [op2Error, setOp2Error] = React.useState('');
-	const [op3, setOp3] = React.useState('');
-	const [op3Error, setOp3Error] = React.useState('');
-	const [op4, setOp4] = React.useState('');
-	const [op4Error, setOp4Error] = React.useState('');
-	const [submitResponse, setSubmit] = React.useState('');
-	const errorText = "This field cannot be empty";
-
+	const [alert, setAlert] = React.useState(false);
+	const [alertText, setAlertText] = React.useState('');
 	const [loading, setLoading] = React.useState(true);
 	const columns = [
 		{ field: 'qid', title: 'Q_ID', minwidth: 10 },
@@ -43,44 +22,49 @@ export default function AllQues(){
 	const [rows, setRows] = React.useState([]);
 
 	const update = async(ndata, odata) => {
-		console.log(ndata, odata);
 		if(ndata.qid === odata.qid){
-
 			let error = false;
 			if(ndata.ques.length === 0){
 				error = true;
-				console.log("No errors 1");
+				setAlert(true);
+				setAlertText('Fields cannot be empty');
 			}
 			if(ndata.op1.length === 0){
 				error = true;
-				console.log("No errors 2");
+				setAlert(true);
+				setAlertText('Fields cannot be empty');
 			}
 			if(ndata.op2.length === 0){
 				error = true;
-				console.log("No errors 3");
+				setAlert(true);
+				setAlertText('Fields cannot be empty');
 			}
 			if(ndata.op3.length === 0){
 				error = true;
-				console.log("No errors 4");
+				setAlert(true);
+				setAlertText('Fields cannot be empty');
 			}
 			if(ndata.op4.length === 0){
 				error = true;
-				console.log("No errors 5");
+				setAlert(true);
+				setAlertText('Fields cannot be empty');
 			}
 			if(ndata.correct.length === 0){
 				error = true;
-				console.log("No errors 6");
+				setAlert(true);
+				setAlertText('Fields cannot be empty');
 			}
 			if(ndata.op1 === ndata.op2 || ndata.op1=== ndata.op3 || ndata.op1 === ndata.op4 || ndata.op2 === ndata.op3 || ndata.op2 === ndata.op4 || ndata.op3 === ndata.op4){
 				error = true;
-				console.log("No errors 7");
+				setAlert(true);
+				setAlertText('Options cannot be repeated');
 			}
 			if(!(ndata.correct === ndata.op1 || ndata.correct === ndata.op2 || ndata.correct === ndata.op3 || ndata.correct === ndata.op4)){
 				error = true;
-				console.log("No errors 8");
+				setAlert(true);
+				setAlertText('Correct option must be from provided options');
 			}
 			if(!error){
-				console.log("No errors");
 				let token = localStorage.getItem('authToken');
 				let url = `https://scholastic-quiz-app.herokuapp.com/questions/${ndata.qid}`
 				let data = {
@@ -116,11 +100,16 @@ export default function AllQues(){
 					console.log(error);
 				}
 			}
+			else{
+				setTimeout(() => {
+					setAlert(false);
+					setAlertText('');
+				}, 5000);
+			}
 		}
 	}
 
 	const del = async(data) => {
-		console.log(data);
 		let token = localStorage.getItem('authToken');
 		let url = `https://scholastic-quiz-app.herokuapp.com/questions/${data.qid}`;
 		let response = null;
@@ -133,7 +122,6 @@ export default function AllQues(){
 			}).then(res => {
 				response = res;
 			});
-			console.log(response);
 			if(response.status === 204){
 				getAll();
 			}
@@ -160,8 +148,6 @@ export default function AllQues(){
 			}).then(res => {
 				response = res;
 			});
-			console.log(response);
-			console.log(rows);
 			if(response.status === 200){
 				for(i = 0; i < response.data.length; i++){
 					var id = response.data[i]._id;
@@ -175,7 +161,6 @@ export default function AllQues(){
 					data = [...data, createData(id,ques,o1,o2,o3,o4,correct)]
 				}
 				setRows(data);
-				console.log(rows);
 			}
 
 		} catch(error) {
@@ -191,12 +176,13 @@ export default function AllQues(){
     return(
         loading? <Loading />
         :
-        <div style={{display:'flex', justifyContent: 'center'}}>
-            <MaterialTable
+        <Grid container spacing={3} style={{display:'flex', justifyContent: 'center', maxWidth: '100%'}}>
+			<Grid item xs={12} style={{display: 'flex', justifyContent: 'center', marginLeft: 25}}>
+			<MaterialTable
 			 title="Questions List"
 			 columns={columns}
 			 data = {rows}
-			 style={{width:'90%', backgroundColor: '#4d4d4d', color: '#fff'}}
+			 style={{width:'98%', backgroundColor: '#4d4d4d', color: '#fff'}}
 			 options={{
 				 headerStyle: {
 					backgroundColor: '#353535',
@@ -223,94 +209,11 @@ export default function AllQues(){
 				  }),
 			  }}
 			 />	
-			
-
-			{/* <Button variant="outlined" className="btn-red" onClick={handleClickOpen}>
-				Add New Question
-			</Button>
-
-			<Dialog PaperProps={{ style: { backgroundColor: '#2d2d2d', color: '#cfcfcf' } }} open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-				<DialogTitle id="form-dialog-title">Add Question</DialogTitle>
-				<DialogContent>
-					<TextInput
-						autoFocus
-						error={quesError.length === 0? false: true}
-						helperText={quesError.length === 0? null: quesError}
-						margin="dense"
-						id="ques"
-						label="Question"
-						type="text"
-						fullWidth
-						variant="outlined"
-						value={ques}
-						onChange={handleQchange}
-					/>
-					<TextInput
-						error={op1Error.length === 0? false: true}
-						helperText={op1Error.length === 0? null: op1Error}
-						margin="dense"
-						id="op1"
-						label="Option 1"
-						type="text"
-						fullWidth
-						variant="outlined"
-						onChange={handle1change}
-					/>
-					<TextInput
-						error={op2Error.length === 0? false: true}
-						helperText={op2Error.length === 0? null: op2Error}
-						margin="dense"
-						id="op2"
-						label="Option 2"
-						type="text"
-						fullWidth
-						variant="outlined"
-						onChange={handle2change}
-					/>
-					<TextInput
-						error={op3Error.length === 0? false: true}
-						helperText={op3Error.length === 0? null: op3Error}
-						margin="dense"
-						id="op3"
-						label="Option 3"
-						type="text"
-						fullWidth
-						variant="outlined"
-						onChange={handle3change}
-					/>
-					<TextInput
-						error={op4Error.length === 0? false: true}
-						helperText={op4Error.length === 0? null: op4Error}
-						margin="dense"
-						id="op4"
-						label="Option 4"
-						type="text"
-						fullWidth
-						variant="outlined"
-						onChange={handle4change}
-					/>
-					<FormControl component="fieldset">
-						<FormLabel style={{ color: '#ffa2000', paddingTop: 20 }} component="legend">Correct Option</FormLabel>
-						<p style={{color: '#f44336', fontSize: 14, margin: 0}}>{valueError}</p>
-						<RadioGroup aria-label="correct-choice" value={value} onChange={handleChange} >
-							<FormControlLabel value="op1" control={<Radio style={{color: '#ffa2000'}} />} label="Option 1" />
-							<FormControlLabel value="op2" control={<Radio style={{color: '#ffa2000'}} />} label="Option 2" />
-							<FormControlLabel value="op3" control={<Radio style={{color: '#ffa2000'}} />} label="Option 3" />
-							<FormControlLabel value="op4" control={<Radio style={{color: '#ffa2000'}} />} label="Option 4" />
-						</RadioGroup>
-					</FormControl>
-					<p style={{color: '#0f0', fontSize: 14, margin: 0}}>{submitResponse}</p>
-
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleClose} className="btn-orange">
-						Cancel
-				</Button>
-					<Button onClick={handleSubmit} className="btn-orange">
-						Submit
-				</Button>
-				</DialogActions>
-			</Dialog> */}
-		</div>
+			</Grid>
+            
+			<Grid item xs={10}>
+				{alert === true? <Alert severity="error">{alertText}</Alert>: null}
+			</Grid>
+		</Grid>
     )
 }
