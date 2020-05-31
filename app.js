3fdd40edf7caf02f445143dd7c0fa8e670644352
Jugal Bhatt <55304795+jugalbhatt123@users.noreply.github.com>
2020-05-31 23:08:14 +0530

    allow cors globally

--- app.js
+++ app.js
@@ -25,6 +25,21 @@ app.use(Questionrouter)
 app.use(QuestionCCrouter)
 app.use('/api/user',Authrouter)
 
+//allow CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
