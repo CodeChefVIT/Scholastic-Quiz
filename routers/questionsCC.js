d78b41e276c844cec74be350c034ca989c90aeb0
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2020-05-31 18:42:54 +0530

    block user

--- routers/questionsCC.js
+++ routers/questionsCC.js
@@ -30,7 +30,7 @@ router.post('/questionsCC',verify,adminAccess, async (req, res) => {
     }
 })
 
-router.get('/getCC',verify, async (req, res) => {
+router.get('/getCC',verify,isBlocked, async (req, res) => {
     // console.log(req.user.user.noOfRefresh)
     var mainQuestions= []
     for(i=0;i<10;i++){
@@ -86,5 +86,30 @@ router.get('/getCC',verify, async (req, res) => {
         res.status(400).send(err)
     } 
 })
+
+
+async function isBlocked(req,res,next){
+    // var user = req.user.user
+    //     console.log(req.user.user._id)
+    //     console.log(req.user.user.noOfRefresh)
+    console.log(req.user.user)
+        const _id=req.user.user._id
+        var user= await User.findOne({_id})
+         var x; 
+         x=user.noOfRefresh+1;
+       // console.log(x)
+    await User.updateOne({_id:user._id},{$set:{"noOfRefresh":x}})
+     user= await User.findOne({_id})
+  // console.log(user.noOfRefresh)
+      if(user.noOfRefresh>1){
+        await User.updateOne({_id:user._id},{$set:{isBlocked:true}})
+      }
+      if(!user.isBlocked)
+            next()
+      else{
+          res.status(403).send("You are blocked from taking the quiz")
+      }
+
+  }
 
 module.exports = router
